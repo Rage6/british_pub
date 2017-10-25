82126b586bf0726c1546a944170f758a415411ef
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2017-10-25 18:17:34 -0400

    option folders with functions

--- british_pub_frontend/views/app.js
+++ british_pub_frontend/views/app.js
@@ -6,9 +6,48 @@ app.controller('mainController', ['$http', function($http) {
   // console.log("The angular module is functioning.")
 
   this.menuDropOpen = false;
+  this.newsDropOpen = false;
+  this.drinksDropOpen = false;
+  this.totalMenuDropOpen = false;
+  this.whereDropOpen = false;
+  this.historyDropOpen = false;
+  this.contactsDropOpen = false;
 
   this.openAndCloseMenu = () => {
     this.menuDropOpen = ! this.menuDropOpen;
+  }
+
+  this.allFalse = () => {
+    this.newsDropOpen = false;
+    this.drinksDropOpen = false;
+    this.totalMenuDropOpen = false;
+    this.whereDropOpen = false;
+    this.historyDropOpen = false;
+    this.contactsDropOpen = false;
+  }
+
+  this.newsBox = () => {
+    this.newsDropOpen = ! this.newsDropOpen;
+  }
+
+  this.drinksBox = () => {
+    this.drinksDropOpen = ! this.drinksDropOpen;
+  }
+
+  this.totalMenuBox = () => {
+    this.totalMenuDropOpen = ! this.totalMenuDropOpen;
+  }
+
+  this.whereBox = () => {
+    this.whereDropOpen = ! this.whereDropOpen;
+  }
+
+  this.historyBox = () => {
+    this.historyDropOpen = ! this.historyDropOpen;
+  }
+
+  this.contactsBox = () => {
+    this.contactsDropOpen = ! this.contactsDropOpen;
   }
 
 }]);
